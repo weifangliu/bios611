c6accf96e11759100aaf2620be968bd7b557c3be
weifangliu <weifang.liu2012@gmail.com>
2018-10-08 10:26:21 -0400

    police

---
+++ police.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+
+police <- read.csv(url("https://raw.githubusercontent.com/fivethirtyeight/data/master/police-locals/police-locals.csv"))
+
+police %>% 
+  filter(police_force_size > 1000) %>%
+  ggplot(aes(x=all, y=police_force_size)) + 
+  geom_point()
+
+ggsave('police_plot.png', width=5, height=4)
